a6e864949f9388ec87741eb4ebcfef2a83e3ad31
nattadetspite <ntd.ctnts@gmail.com>
2022-06-10 14:00:46 +0700

    :memo: test todo format

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 /**
- * @todo #TEST-13
+ * @todo #234:15m/DEV This is something to do later
+ *  in one of the next releases. I can't figure out
+ *  how to implement it now, that's why the puzzle.
  */
 
-console.log("test");
+console.log("test format");
